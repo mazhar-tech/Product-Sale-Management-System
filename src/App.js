0e5150aaa1520fca086825c5208897b41d145211
Muhammad Mazhar Iqbal <this.miqbal@gmail.com>
2022-12-17 01:02:24 +0500

    Create app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import SignIn from "./components/signin";
+import SignUp from "./components/signUp";
+import Dashboard from "./components/dashboard/Dashboard";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Dashboard />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/signUp" element={<SignUp />}></Route>
+      </Routes>
     </div>
   );
 }
